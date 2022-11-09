0b6dcd583e1ede86b4bf10e82282f65e89931aa7
joonyeonkimdev <joonyeonkim.dev@gmail.com>
2022-11-09 14:56:56 +0900

    프로젝트 패키지별 클래스 생성 221109

---
+++ src/main/java/com/wannaattention/www/dao/UserDAO.java
@@ -0,0 +1,8 @@
+package com.wannaattention.www.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserDAO {
+
+}
---
+++ src/main/java/com/wannaattention/www/util/Sitemesh.java
@@ -0,0 +1,5 @@
+package com.wannaattention.www.util;
+
+public class Sitemesh {
+
+}
