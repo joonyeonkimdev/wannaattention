db0f295b6ed524f2bca8331bf7d79a7c6f78828b
joonyeonkimdev <joonyeonkim.dev@gmail.com>
2022-11-10 03:06:48 +0900

    SiteMeshFilter 설정 및 화면 Layout 제작 221110

--- src/main/java/com/wannaattention/www/controller/BoardContoller.java
+++ src/main/java/com/wannaattention/www/controller/BoardContoller.java
@@ -2,10 +2,15 @@ package com.wannaattention.www.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("board")
 public class BoardContoller {
-
+	@RequestMapping("animalList")
+	public ModelAndView animalList() {
+		ModelAndView mav = new ModelAndView();
+		return mav;
+	}
 	
 }
--- src/main/java/com/wannaattention/www/util/Sitemesh.java
+++
@@ -1,5 +0,0 @@
-package com.wannaattention.www.util;
-
-public class Sitemesh {
-
-}
