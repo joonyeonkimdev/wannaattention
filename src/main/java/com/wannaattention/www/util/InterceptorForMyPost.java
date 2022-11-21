537043f9c7d9598984eaffa1b196815083dd4916
joonyeonkimdev <joonyeonkim.dev@gmail.com>
2022-11-21 23:22:52 +0900

    게시글 수정/삭제 접근 제한 추가 221121

--- src/main/java/com/wannaattention/www/util/InterceptorForMyPost.java
+++ src/main/java/com/wannaattention/www/util/InterceptorForMyPost.java
@@ -8,8 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import com.wannaattention.www.dao.AnimalDAO;
+import com.wannaattention.www.dao.CommunityDAO;
 import com.wannaattention.www.dao.MissingDAO;
 import com.wannaattention.www.vo.Animal;
+import com.wannaattention.www.vo.Board;
 import com.wannaattention.www.vo.MissingAnimal;
 import com.wannaattention.www.vo.User;
 
@@ -18,13 +20,15 @@ public class InterceptorForMyPost extends HandlerInterceptorAdapter {
 	private AnimalDAO animalDao;
 	@Autowired
 	private MissingDAO missingDao;
+	@Autowired
+	private CommunityDAO communityDao;
 	
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 		HttpSession session = request.getSession();
 		User loginUser = (User)session.getAttribute("loginUser");
 		
-		// 입양 동물 수정 권한
+		// 입양 동물 수정/삭제 권한
 		if (request.getParameter("animalNum") == null) {
 			throw new InterceptedException("올바르지 않은 접근입니다.", "/animal/animalList");
 		}
@@ -34,7 +38,7 @@ public class InterceptorForMyPost extends HandlerInterceptorAdapter {
 			throw new InterceptedException("접근 권한이 없습니다.", "/animal/animalList");
 		}
 		
-		// 실종 동물 수정 권한
+		// 실종 동물 수정/삭제 권한
 		if (request.getParameter("missingAnimalNum") == null) {
 			throw new InterceptedException("올바르지 않은 접근입니다.", "/missing/missingList");
 		}
@@ -44,6 +48,15 @@ public class InterceptorForMyPost extends HandlerInterceptorAdapter {
 			throw new InterceptedException("접근 권한이 없습니다.", "/missing/missingList");
 		}
 		
+		// 게시물 수정/삭제 권한
+		if (request.getParameter("boardNum") == null) {
+			throw new InterceptedException("올바르지 않은 접근입니다.", "/community/boardList");
+		}
+		Integer boardNum = Integer.parseInt(request.getParameter("boardNum"));
+		Board board = communityDao.selectBoardByBN(boardNum);
+		if (loginUser.getUserNum() != board.getWriterNum()) {
+			throw new InterceptedException("접근 권한이 없습니다.", "/community/boardList");
+		}
 		return super.preHandle(request, response, handler);
 	}
 	
